7f808c2220e1dff6e39c291c87ccc5630309c1fe
leomon17 <63978802+leomon17@users.noreply.github.com>
2022-10-11 15:08:16 -0500

    Update sw.js

--- practica2/sw.js
+++ practica2/sw.js
@@ -1,62 +1,35 @@
 //console.log("HOLA MUNDO DESDE EL SW!!");
 self.addEventListener('install',(event)=>{
     console.log("SW: Instalado");
-    /*
-    const myPromise = new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            console.log('Instalaciones finalizadas');
-            resolve('ok')
-        },3000);
-    });
-    event.waitUntil(myPromise)*/
 });
 
 self.addEventListener('activate', (event)=>{
     console.log("SW: Activado");
 });
 
+
 self.addEventListener('fetch', (event)=>{
     console.log(event.request.url);
-    fetch(event.request.url).then((resp) => {
-        console.log(resp);
-        if (resp.ok) {
-            event.respondWith(resp);
-        } else {
-            console.log("generico");
-            const generic = fetch("images/imagen2.jpg");
-            event.respondWith(generic);
-        }
-        //event.respondWith(resp.ok?resp:fetch('images/imagen2.jpg'))
-    }).catch((err) =>{
-        console.log(err);
-    });
 
-    const resp = fetch(event.request.url);
-    console.log(resp);
-    event.respondWith(resp);
-
-    
-    if (event.request.url.includes('style.css')) {
+    if(event.request.url.includes('style.css')){
         const respuesta = new Response(
             `body{
-                color:red;
-                background-color:#000;
+                color: white;
+                background-color: #3c2348;
             }`,
             {
                 headers:{
                     'Content-Type':'text/css'
                 }
             }
-        )
+        );
         event.respondWith(respuesta);
     }
-
-    if (event.request.url.includes('imagen1.jpg')) {
-        const respuesta = fetch("images/imagen2.jpg");
-        event.respondWith(respuesta);
+    if(event.request.url.includes('.jpg')){
+        let res = fetch('images/imagen2.jpg')
+        event.respondWith(res)
     }
-});
 
-
+})
     
    
