c2eb43eb08c7b66c3b76748538a28991a791f943
leomon17 <63978802+leomon17@users.noreply.github.com>
2022-10-11 15:32:09 -0500

    Update app.js

--- practica2/js/app.js
+++ practica2/js/app.js
@@ -2,6 +2,7 @@ console.log("HOLA MUNDO 2");
 if (navigator.serviceWorker) {
     console.log("soporta el SW");
     //Identificar si estoy en local o en github
+   navigator.serviceWorker.register('sw.js')
     navigator.serviceWorker.register('practica2/sw.js')
 }else{
     console.log("no soporta");
